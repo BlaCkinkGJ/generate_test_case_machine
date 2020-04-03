887891f7274881e098023429fcd66bbd8fd656bb
BlaCkinkGJ <ss5kijun@gmail.com>
2020-04-04 03:37:49 +0900

    More feature to generator.py

--- generator.py
+++ generator.py
@@ -5,20 +5,22 @@ import random
 from tqdm import tqdm
 import pprint
 
-NR_TEST_CASE = int(1e5)
+NR_TEST_CASE = int(5e6)
 INSERT = 0
 REMOVE = 1
 UPDATE = 2
 SEARCH = 3
 
-MAX_NR_ENTRY = 100000
+MAX_NR_ENTRY = 10000000
 
-SEARCH_DOMINANT = [INSERT, REMOVE] + [SEARCH for _ in range(8)]
-REMOVE_DOMINANT = [INSERT, SEARCH] + [REMOVE for _ in range(8)]
-INSERT_DOMINANT = [REMOVE, SEARCH] + [INSERT for _ in range(8)]
+SEARCH_DOMINANT = [INSERT, REMOVE] + [SEARCH for _ in range(3)]
+REMOVE_DOMINANT = [INSERT, SEARCH] + [REMOVE for _ in range(3)]
+INSERT_DOMINANT = [REMOVE, SEARCH] + [INSERT for _ in range(3)]
 UNIFORM_DISTRIB = [INSERT, REMOVE, SEARCH]
 
 COMMAND_LIST = UNIFORM_DISTRIB
+
+ERROR_RATE = 0.33  # 1%
 
 # per char [] > 64
 if __name__ == "__main__":
@@ -50,9 +52,15 @@ if __name__ == "__main__":
             if len(id_set) <= 0:
                 state = INSERT
                 continue
-            student_id = (random.sample(id_set, 1))[0]
-            test_file.write("REMOVE,{}\n".format(student_id))
-            id_set.remove(student_id)
+            command = "REMOVE" if random.random() > ERROR_RATE else ""
+            student_id = (
+                (random.sample(id_set, 1))[0]
+                if random.random() > ERROR_RATE
+                else random.randint(201400000, 201499999)
+            )
+            test_file.write("{},{}\n".format(command, student_id))
+            if student_id in id_set:
+                id_set.remove(student_id)
             nr_remove += 1
 
         elif state == UPDATE:
@@ -60,14 +68,23 @@ if __name__ == "__main__":
                 state = INSERT
                 continue
 
-            student_id = (random.sample(id_set, 1))[0]
-            name = fake.first_name()
-            bban = fake.bban()
-            email = fake.email()
+            command = "UPDATE" if random.random() > ERROR_RATE else ""
+            student_id = (
+                (random.sample(id_set, 1))[0]
+                if random.random() > ERROR_RATE
+                else random.randint(201400000, 201499999)
+            )
+            name = fake.first_name() if random.random() > ERROR_RATE else ""
+            bban = fake.bban() if random.random() > ERROR_RATE else ""
+            email = fake.email() if random.random() > ERROR_RATE else ""
 
             test_file.write(
-                "UPDATE,{},{},{},{}\n".format(
-                    student_id, fake.first_name(), fake.bban(), fake.ascii_email(),
+                "{},{},{},{},{}\n".format(
+                    command,
+                    student_id,
+                    fake.first_name(),
+                    fake.bban(),
+                    fake.ascii_email(),
                 )
             )
             nr_update += 1
@@ -76,8 +93,13 @@ if __name__ == "__main__":
             if len(id_set) <= 0:
                 state = INSERT
                 continue
-            student_id = (random.sample(id_set, 1))[0]
-            test_file.write("SEARCH,{}\n".format(student_id))
+            student_id = (
+                (random.sample(id_set, 1))[0]
+                if random.random() > ERROR_RATE
+                else random.randint(201400000, 201499999)
+            )
+            command = "SEARCH" if random.random() > ERROR_RATE else ""
+            test_file.write("{},{}\n".format(command, student_id))
             nr_search += 1
 
         elif state == INSERT:
@@ -90,15 +112,18 @@ if __name__ == "__main__":
                     )
                 )[0]
                 continue
-            id_set.add(student_id)
 
-            name = fake.first_name()
-            bban = fake.bban()
-            email = fake.email()
+            command = "INSERT" if random.random() > ERROR_RATE else ""
+            student_id = student_id if random.random() > ERROR_RATE else ""
+            name = fake.first_name() if random.random() > ERROR_RATE else ""
+            bban = fake.bban() if random.random() > ERROR_RATE else ""
+            email = fake.email() if random.random() > ERROR_RATE else ""
 
             test_file.write(
-                "INSERT,{},{},{},{}\n".format(student_id, name, bban, email)
+                "{},{},{},{},{}\n".format(command, student_id, name, bban, email)
             )
+            if student_id != "":
+                id_set.add(student_id)
             nr_insert += 1
 
         idx += 1
